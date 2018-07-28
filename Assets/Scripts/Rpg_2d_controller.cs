19f8c94a498830c640222b6b74611ee21b517d44
lucas nielsen <l>
2018-07-28 11:49:38 +0200

    made movement

--- Assets/Scripts/Rpg_2d_controller.cs
+++ Assets/Scripts/Rpg_2d_controller.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class Rpg_2d_controller : MonoBehaviour
 {
     private bool isAttacking = false;
+
+    [SerializeField]
     private float speed = 5f;
+
     private float horizontal = 0;
     private float vertical = 0;
     private Animator animation;
@@ -31,6 +35,15 @@ public class Rpg_2d_controller : MonoBehaviour
             animation.SetBool("Walking", true);
             animation.SetFloat("Horizontal", horizontal);
             animation.SetFloat("Vertical", vertical);
+            transform.position = Move(horizontal, vertical);
         }
+    }
+
+    private Vector3 Move(float horizontalInput, float verticalInput)
+    {
+        Vector3 newPosition = transform.position;
+        newPosition.x += horizontalInput * speed * Time.deltaTime;
+        newPosition.y += verticalInput * speed * Time.deltaTime;
+        return newPosition;
     }
 }
